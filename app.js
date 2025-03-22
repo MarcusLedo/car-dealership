f1fc67f80c4a1b5933855099921fe1e4daf326d2
Marcus <sabaledo27@hotmail.com>
2025-03-22 00:19:46 -0300

    Initialize express app

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 import { addData } from "./DB/firebase.js";
+import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-main();
+const __file = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__file);
+const app = express();
 
-async function main() {
-  const testData = {
-    totalValue: 432,
-    seller: "I'm alive",
-    discount: 293,
-  };
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", "ejs");
 
-  await addData(testData);
-}
+app.get("/", (req, res) => {
+  console.log("Here!");
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000.");
+});
