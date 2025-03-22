010bb4c5b44551b5260635d3534145851cb1332a
Marcus <sabaledo27@hotmail.com>
2025-03-22 02:33:43 -0300

    Create public folder and beautify the transaction form a little bit

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   console.log("Here!");
