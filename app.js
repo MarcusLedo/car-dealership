d41b8e9bac6bd29ff6ab4718f8d01a2fa25c15e3
Marcus <sabaledo27@hotmail.com>
2025-03-22 02:03:24 -0300

    Enable the creation of new transactions

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   console.log("Here!");
   res.render("home");
@@ -18,13 +20,19 @@ app.get("/", (req, res) => {
 app.get("/transactions", async (req, res) => {
   //List all transaction in the DB
   const data = await getData();
-  console.log(data);
+  //console.log(data);
 
   res.render("transactions", { data });
 });
 
-app.get("/transactions/new", (req, res) => {
+app.get("/transaction/new", (req, res) => {
   res.render("newTransaction");
+});
+
+app.post("/transactions", async (req, res) => {
+  const data = req.body;
+  await addData(data);
+  res.redirect("/transactions");
 });
 
 app.listen(3000, () => {
